bd32302875161ba5f9d4e8fca97616e39a65d56d
Neil Alexander <neilalex94@gmail.com>
2024-05-10 12:05:11 +0100

    Fixed player switching when choice made, cameras now change positon

--- PhD Prototype/Assets/Scripts/ChoiceMade.cs
+++ PhD Prototype/Assets/Scripts/ChoiceMade.cs
@@ -8,12 +8,30 @@ public class ChoiceMade : MonoBehaviour
 
     public GameObject newPlayer;
 
+    // Active Cameras
+    public GameObject lookForward;
+    public GameObject lookLeft;
+    public GameObject lookRight;
+    public GameObject overheadCam;
+
+    // Dummy Cameras to take position from
+    public GameObject dummyLookForward;
+    public GameObject dummyLookLeft;
+    public GameObject dummyLookRight;
+    public GameObject dummyOverheadCam;
+
     public void SwitchPlayer()
     {
+        // Set all active cameras to child of new player
         foreach (GameObject camerasToMove in cameraList)
         {
             camerasToMove.transform.parent = newPlayer.transform;
-            camerasToMove.transform.position = new Vector3(0, 2, -3);
         }
+
+        // Set new camera positions based on position of dummy cameras on new player
+        lookForward.transform.position = dummyLookForward.transform.position;
+        lookLeft.transform.position = dummyLookLeft.transform.position;
+        lookRight.transform.position = dummyLookRight.transform.position;
+        overheadCam.transform.position = dummyOverheadCam.transform.position;
     }
 }
