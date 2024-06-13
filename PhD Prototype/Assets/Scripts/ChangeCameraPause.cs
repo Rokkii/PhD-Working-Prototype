c481c0b4dfcaa45ba88d78f082abb85e2a0045fb
Neil Alexander <neilalex94@gmail.com>
2024-06-13 14:25:23 +0100

    Fixed game world speed when camera change pause called

--- PhD Prototype/Assets/Scripts/ChangeCameraPause.cs
+++ PhD Prototype/Assets/Scripts/ChangeCameraPause.cs
@@ -17,12 +17,7 @@ public class ChangeCameraPause : MonoBehaviour
         gameSpeed = DifficultySetting.playerSpeedSelection;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Start coroutine when script started
     public void StartCameraPause()
     {
         StartCoroutine(CameraChangePause(pauseTime));
@@ -33,15 +28,28 @@ public class ChangeCameraPause : MonoBehaviour
         print("Starting camera pause of: " + pauseTime + " seconds");
         Time.timeScale = 0f;
         float pauseEndTime = Time.realtimeSinceStartup + pauseTime;
+
+        // While time since scene start up is less than pause time, keep game speed = 0
         while (Time.realtimeSinceStartup < pauseEndTime)
         {
             yield return 0;
         }
+
+        // Reset game speed
         Time.timeScale = gameSpeed;
         print("Pause finished, resetting camera and setting game speed to: " + gameSpeed);
+
+        // Enable/Disable array of gameobjects
         foreach(GameObject disableObjects in disableList)
             disableObjects.SetActive(false);
         foreach (GameObject enableObjects in enableList)
             enableObjects.SetActive(true);
+    }
+
+    // Reset Game World Speed when called
+    public void ResetGameWorldSpeed()
+    {
+        Time.timeScale = gameSpeed;
+        print("Game Speed now: " + Time.timeScale);
     }
 }
