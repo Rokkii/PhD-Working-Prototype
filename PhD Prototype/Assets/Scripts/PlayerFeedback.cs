00bfb0ac8a4b711bf9545655a21fe432af73c489
Neil Alexander <neilalex94@gmail.com>
2024-06-20 17:06:32 +0100

    Added player action function, fixed to display actions and feedback

--- PhD Prototype/Assets/Scripts/PlayerFeedback.cs
+++ PhD Prototype/Assets/Scripts/PlayerFeedback.cs
@@ -9,7 +9,12 @@ public class PlayerFeedback : MonoBehaviour
     public TMP_Text feedbackDisplay;            // Game text object to display player feedback
     public string sceneFeedback;                // String for specific feedback set in-client
 
+    public bool addPlayerAction = true;
+    public TMP_Text actionDisplay;
+    public string playerAction;
+
     public static string finalFeedbackString = "No Feedback Yet";
+    public static string finalActionString = "No Actions Yet";
 
     // If the scene conclusion has feedback assigned, add it to the player's feedback array
     public void AddPlayerFeedback()
@@ -30,10 +35,35 @@ public class PlayerFeedback : MonoBehaviour
         }
     }
 
+    // If the scene conclusion has feedback assigned, add player choice to actions
+    public void AddPlayerAction()
+    {
+        // If final feedback string is not default, just add next feedback element
+        if (finalActionString != "No Feedback Yet" && addPlayerAction == true)
+        {
+            finalActionString += playerAction + "\n";
+            print("Adding feedback: " + playerAction);
+        }
+        // If final feedback string is set to default, clear and add initial feedback element
+        if (finalActionString == "No Feedback Yet" && addPlayerAction == true)
+        {
+            finalActionString = "";
+            print("No Feedback detected, clearing string");
+            finalActionString += playerAction + "\n";
+            print("Adding feedback: " + playerAction);
+        }
+    }
+
     // Show all generated player feedback on the in-client game text object
     public void ShowFeedback()
     {
         feedbackDisplay.text = finalFeedbackString.ToString();
+    }
+
+    // Show all generated player actions on the in-client game text object
+    public void ShowActions()
+    {
+        actionDisplay.text = finalActionString.ToString();
     }
 
     // Reset player feedback array when called
